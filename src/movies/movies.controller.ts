7edf02d5cb80ae08b540d2eaae72d2c039df088f
cocohan11 <rlagksdl96@naver.com>
2024-12-12 19:50:28 +0900

    데코레이터 - API 만들 때 필요한 여러 데코레이터들 연습 - Body, Query 등등

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 
 @Controller('movies') // Entry 포인트 역할 (http://localhost:3000/movies)
 export class MoviesController {
@@ -7,27 +7,36 @@ export class MoviesController {
     getAll() {
         return 'This will return all movies'
     }
+    
+    @Get("/search")
+    search(@Query('year') searchingYear: number) { // 파라미터
+        return `We are searching for a movie made after: ${searchingYear}`
+    }
 
-    @Get("/:id")
+    @Get(":id")
     getOne(@Param('id') id: string) { // 파라미터
         return `This will return one movies with the id: ${id}`
     }
 
     @Post()
-    create() {
-        return 'This will create a movie'
+    create(@Body() movieData) {
+        console.log(movieData)
+        return movieData;
     }
     
-    @Delete("/:id")
+    @Delete(":id")
     remove(@Param('id') id: string) { 
         return `This will delete a movies with the id: ${id}`
     }
     
     // @Put -> 전체 수정
     // @Patch -> 일부부 수정
-    @Patch("/:id")
-    patch(@Param('id') id: string) { 
-        return `This will update a movies with the id: ${id}`
+    @Patch(":id")
+    patch(@Param('id') movieId: string, @Body() updateData) { 
+        return {
+            updateData : movieId,
+            ...updateData,
+        }
     }
 
 
