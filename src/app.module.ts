fbe2b2672aaf377ff068a78bf86259ca33db6a3f
cocohan11 <rlagksdl96@naver.com>
2024-12-17 21:36:07 +0900

    2.0강 시작 전 커밋

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
+import { MoviesModule } from './movies/movies.module';
+
 
 @Module({ // 모듈이란? 📁 서류철처럼 관련된 것들을 한 폴더에 모아두는 것
-  imports: [],
-  controllers: [MoviesController], // 하는 일 : url + 함수 매핑 (express의 라우터와 같음)
+  imports: [MoviesModule],
+  controllers: [], // 하는 일 : url + 함수 매핑 (express의 라우터와 같음)
   providers: [],
 })
 export class AppModule {}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,43 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { Controller, Get} from '@nestjs/common';
+import { MoviesService } from './movies.service';
 
 @Controller('movies') // Entry 포인트 역할 (http://localhost:3000/movies)
 export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) {} // 비지니스로직 함수 불러오기기
 
     @Get()
-    getAll() {
-        return 'This will return all movies'
+    getHello() {
+        return this.moviesService.getHello();
     }
-    
-    @Get("/search")
-    search(@Query('year') searchingYear: number) { // 파라미터
-        return `We are searching for a movie made after: ${searchingYear}`
-    }
-
-    @Get(":id")
-    getOne(@Param('id') id: string) { // 파라미터
-        return `This will return one movies with the id: ${id}`
-    }
-
-    @Post()
-    create(@Body() movieData) {
-        console.log(movieData)
-        return movieData;
-    }
-    
-    @Delete(":id")
-    remove(@Param('id') id: string) { 
-        return `This will delete a movies with the id: ${id}`
-    }
-    
-    // @Put -> 전체 수정
-    // @Patch -> 일부부 수정
-    @Patch(":id")
-    patch(@Param('id') movieId: string, @Body() updateData) { 
-        return {
-            updateData : movieId,
-            ...updateData,
-        }
-    }
-
-
 }
