3fce77618dc0cc7f2f29fa66e6f508ff10d70579
cocohan11 <rlagksdl96@naver.com>
2024-12-17 21:53:04 +0900

    2.1 수업 전 커밋

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesModule } from './movies/movies.module';
+import { MoviesController } from './movies/movies.controller';
 
 
 @Module({ // 모듈이란? 📁 서류철처럼 관련된 것들을 한 폴더에 모아두는 것
-  imports: [MoviesModule],
-  controllers: [], // 하는 일 : url + 함수 매핑 (express의 라우터와 같음)
+  imports: [],
+  controllers: [MoviesController], // 하는 일 : url + 함수 매핑 (express의 라우터와 같음)
   providers: [],
 })
 export class AppModule {}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get} from '@nestjs/common';
-import { MoviesService } from './movies.service';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
-@Controller('movies') // Entry 포인트 역할 (http://localhost:3000/movies)
+@Controller('movies') // Entry point 
 export class MoviesController {
-    constructor(private readonly moviesService: MoviesService) {} // 비지니스로직 함수 불러오기기
 
     @Get()
-    getHello() {
-        return this.moviesService.getHello();
+    getAll() {
+        return "This will return all movies";
     }
+
+    @Get("/:id")
+    getOne(@Param('id') movieId : string) {
+        return `This will return one movie ${movieId}`;
+    }
+
+    @Post()
+    create() {
+        return "This will create a movie";
+    }
+
+    @Delete('/:id')
+    remove(@Param('id') movieId: string) {
+        return `This will delete a movie with the id: ${movieId}`;
+    }
+
+    @Patch('/:id')
+    update(@Param('id') movieId: string) {
+        return `This will patch a movie with the id: ${movieId}`;
+    }
+
 }
