64571ac6c0913e52849197bb2da2078f8b394083
cocohan11 <rlagksdl96@naver.com>
2025-01-05 22:18:12 +0900

    test epect type and equal

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,6 +1,7 @@
 // src/movies/movies.service.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -17,6 +18,35 @@ describe('MoviesService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll()
+      expect(result).toBeInstanceOf(Array); //배열이다. 
+    })
+  })
+
+  describe('getOne', () => {
+
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000
+      })
+      const movie = service.getOne(1); 
+      expect(movie).toBeDefined() //빈값아님
+      expect(movie.id).toEqual(1) 
+    })
+
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual('Movie with ID: 999 not found..');
+      }
+    })
+  })
 
 
 });
